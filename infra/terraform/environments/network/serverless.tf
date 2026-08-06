39ad6b59054b13e59af4b30000a0f4a6ded9ac51
Larry Miami <larrymiami94@gmail.com>
2026-08-06 14:58:24 +0300

    style(v0.7c): format serverless connector composition

--- infra/terraform/environments/network/serverless.tf
+++ infra/terraform/environments/network/serverless.tf
@@ -3,14 +3,14 @@ module "serverless_connectors" {
 
   source = "../../modules/serverless-vpc-connector"
 
-  project_id      = var.country_networks[each.key].host_project_id
-  name            = each.value.name
-  region          = each.value.region
-  network_name    = module.country_networks[each.key].network_name
-  ip_cidr_range   = each.value.ip_cidr_range
-  machine_type    = each.value.machine_type
-  min_instances   = each.value.min_instances
-  max_instances   = each.value.max_instances
-  min_throughput  = each.value.min_throughput
-  max_throughput  = each.value.max_throughput
+  project_id     = var.country_networks[each.key].host_project_id
+  name           = each.value.name
+  region         = each.value.region
+  network_name   = module.country_networks[each.key].network_name
+  ip_cidr_range  = each.value.ip_cidr_range
+  machine_type   = each.value.machine_type
+  min_instances  = each.value.min_instances
+  max_instances  = each.value.max_instances
+  min_throughput = each.value.min_throughput
+  max_throughput = each.value.max_throughput
 }
