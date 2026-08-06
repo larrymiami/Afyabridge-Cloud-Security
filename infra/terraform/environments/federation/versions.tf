5c07d49adc090ccb1f81d52563efb9cd2b126bd9
Larry Miami <larrymiami94@gmail.com>
2026-08-06 17:06:59 +0300

    feat(terraform): add federation environment constraints

---
+++ infra/terraform/environments/federation/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = "~> 1.15.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.41.0"
+    }
+  }
+}
+
+provider "google" {}
