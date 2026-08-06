a40eff35e7d1718c65715ebffda4d7923b159db1
Larry Miami <larrymiami94@gmail.com>
2026-08-06 16:34:31 +0300

    feat(v0.7d): expose Cloud Storage outputs

---
+++ infra/terraform/modules/cloud-storage/outputs.tf
@@ -0,0 +1,19 @@
+output "name" {
+  description = "Bucket name."
+  value       = google_storage_bucket.this.name
+}
+
+output "url" {
+  description = "Google Storage URL."
+  value       = google_storage_bucket.this.url
+}
+
+output "self_link" {
+  description = "Bucket self link."
+  value       = google_storage_bucket.this.self_link
+}
+
+output "location" {
+  description = "Bucket location."
+  value       = google_storage_bucket.this.location
+}
