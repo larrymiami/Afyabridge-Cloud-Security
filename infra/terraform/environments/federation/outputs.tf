124f49264cb92d5b6dfedae15c9a0c98970282e9
Larry Miami <larrymiami94@gmail.com>
2026-08-06 17:57:14 +0300

    style(terraform): format federation outputs

--- infra/terraform/environments/federation/outputs.tf
+++ infra/terraform/environments/federation/outputs.tf
@@ -1,12 +1,12 @@
 output "github_federation" {
   description = "GitHub Actions workload identity pools, providers, and deployment service-account identifiers."
   value = {
-    plan_pool_name             = module.github_actions_federation.plan_pool_name
-    apply_pool_name            = module.github_actions_federation.apply_pool_name
-    plan_provider_name         = module.github_actions_federation.plan_provider_name
-    apply_provider_name        = module.github_actions_federation.apply_provider_name
-    plan_service_account_email = module.github_actions_federation.plan_service_account_email
+    plan_pool_name              = module.github_actions_federation.plan_pool_name
+    apply_pool_name             = module.github_actions_federation.apply_pool_name
+    plan_provider_name          = module.github_actions_federation.plan_provider_name
+    apply_provider_name         = module.github_actions_federation.apply_provider_name
+    plan_service_account_email  = module.github_actions_federation.plan_service_account_email
     apply_service_account_email = module.github_actions_federation.apply_service_account_email
-    project_number             = module.github_actions_federation.project_number
+    project_number              = module.github_actions_federation.project_number
   }
 }
