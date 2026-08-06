13f8788e77077016f08a38f6988ed0d8792ee76d
Larry Miami <larrymiami94@gmail.com>
2026-08-06 14:58:36 +0300

    style(v0.7c): format serverless connector module

--- infra/terraform/modules/serverless-vpc-connector/main.tf
+++ infra/terraform/modules/serverless-vpc-connector/main.tf
@@ -4,10 +4,10 @@ resource "google_vpc_access_connector" "this" {
   region  = var.region
   network = var.network_name
 
-  ip_cidr_range = var.ip_cidr_range
-  machine_type  = var.machine_type
-  min_instances = var.min_instances
-  max_instances = var.max_instances
+  ip_cidr_range  = var.ip_cidr_range
+  machine_type   = var.machine_type
+  min_instances  = var.min_instances
+  max_instances  = var.max_instances
   min_throughput = var.min_throughput
   max_throughput = var.max_throughput
 }
