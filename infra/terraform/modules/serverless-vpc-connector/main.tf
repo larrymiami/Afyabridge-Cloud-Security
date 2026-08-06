f0337a0ab4321c49076eb98e8ebc89948110119e
Larry Miami <larrymiami94@gmail.com>
2026-08-06 14:54:25 +0300

    feat(v0.7c): implement serverless vpc connector module

---
+++ infra/terraform/modules/serverless-vpc-connector/main.tf
@@ -0,0 +1,13 @@
+resource "google_vpc_access_connector" "this" {
+  project = var.project_id
+  name    = var.name
+  region  = var.region
+  network = var.network_name
+
+  ip_cidr_range = var.ip_cidr_range
+  machine_type  = var.machine_type
+  min_instances = var.min_instances
+  max_instances = var.max_instances
+  min_throughput = var.min_throughput
+  max_throughput = var.max_throughput
+}
