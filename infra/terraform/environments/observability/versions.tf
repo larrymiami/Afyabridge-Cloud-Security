65335e61c8806e069387984eede308fc6e0ca545
Larry Miami <larrymiami94@gmail.com>
2026-08-06 18:11:12 +0300

    feat(logging): add observability environment versions

---
+++ infra/terraform/environments/observability/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.15.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.41.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.logging_project_id
+}
