4ea96594fb93de5e338c86bb2b1a57ee94f634df
Larry Miami <larrymiami94@gmail.com>
2026-08-07 11:10:24 +0300

    feat(edge): expose country edge inventory

---
+++ infra/terraform/environments/edge/outputs.tf
@@ -0,0 +1,12 @@
+output "country_edge_inventory" {
+  description = "Country-isolated regional edge routing inventory."
+  value = {
+    for country, edge in module.country_edges : country => {
+      frontend_address     = edge.frontend_address
+      proxy_only_subnet_id = edge.proxy_only_subnet_id
+      serverless_neg_id    = edge.serverless_neg_id
+      backend_service_id   = edge.backend_service_id
+      url_map_id           = edge.url_map_id
+    }
+  }
+}
